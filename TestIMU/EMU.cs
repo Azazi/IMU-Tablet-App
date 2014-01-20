f792526ee797ea5cb436b33358d57018c41b937d
Daniel Sabourin <sabourid@ucalgary.ca>
2014-01-20 15:29:52 -0700

    Initial commit

---
+++ TestIMU/EMU.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO.Ports;
+using System.Threading;
+
+namespace TestIMU
+{
+    public class EMU
+    {
+        SerialPort port;
+
+        int count = 0;
+
+        string bufferString = "";
+
+        public delegate void DataRecieved(int accelX, int accelY, int accelZ, int gyroX, int gyroY, int gyroZ);
+        public event DataRecieved OnDataRecieved;
+
+        public EMU()
+        {
+            port = new SerialPort("COM7", 115200, Parity.None, 8, StopBits.One);
+            port.DataReceived += new SerialDataReceivedEventHandler(port_DataReceived);
+        }
+
+        public void Start()
+        {
+            port.Open();
+        }
+
+        public void Stop()
+        {
+            port.Close();
+        }
+
+        private void port_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            string data = port.ReadExisting();
+            bufferString += data;
+
+            //Console.WriteLine(bufferString);
+
+            string[] split = bufferString.Split(new string[] { "\r\n", "\n" }, StringSplitOptions.RemoveEmptyEntries);
+            bufferString = "";
+
+            for (int i = 0; i < split.Length; i++)
+            {
+                string line = split[i];
+                string[] split2 = line.Split(',');
+
+                if (split2.Length == 6)
+                {
+                    if (OnDataRecieved != null)
+                    {
+                        try
+                        {
+                            OnDataRecieved(Convert.ToInt32(split2[0]), Convert.ToInt32(split2[1]), Convert.ToInt32(split2[2]), Convert.ToInt32(split2[3]), Convert.ToInt32(split2[4]), Convert.ToInt32(split2[5]));
+                        }
+                        catch (Exception exception)
+                        {
+                            //One of the values of 6 could not be formatted into an int
+                            int a = 0;
+                            //Console.WriteLine(count++ + "\t" + line);
+                        }
+                    }
+                }
+                else
+                {
+                    //This line did not contain 6 entries
+                    if (i == (split.Length - 1))
+                    {
+                        // Adds the last line to the next buffer, since next data collection time will contain the rest of the data.
+                        // Does not take into consideration if the last entry is only '-'
+                        bufferString = line;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ TestIMU/MainWindow.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO.Ports;
+
+namespace TestIMU
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        EMU emu;
+
+        double previousXOrientation = 0;
+        double previousYOrientation = 0;
+        double previousZOrientation = 0;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            emu = new EMU();
+            emu.OnDataRecieved += new EMU.DataRecieved(emu_OnDataRecieved);
+            emu.Start();
+        }
+
+        void emu_OnDataRecieved(int accelX, int accelY, int accelZ, int gyroX, int gyroY, int gyroZ)
+        {
+            double scaleFactor = 15.75467;
+            double sensitivity = 0.0000875;
+
+            //double xrAngle;
+            //double yrAngle;
+            //double zrAngle;
+
+            //double r = Math.Sqrt(accelX * accelX + accelY * accelY + accelZ * accelZ);
+
+            double xOrientation = (gyroX * sensitivity + previousXOrientation);
+            previousXOrientation = xOrientation;
+
+            double yOrientation = (gyroY * sensitivity + previousYOrientation);
+            previousYOrientation = yOrientation;
+
+            double zOrientation = (gyroZ * sensitivity + previousZOrientation);
+            previousZOrientation = zOrientation;
+
+            double scaledXOrientation = scaleFactor * xOrientation;
+            double scaledYOrientation = scaleFactor * yOrientation;
+            double scaledZOrientation = scaleFactor * zOrientation;
+
+            Console.WriteLine(scaledZOrientation + "");
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            emu.Stop();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            previousXOrientation = 0;
+            previousYOrientation = 0;
+            previousZOrientation = 0;
+        }
+    }
+}
